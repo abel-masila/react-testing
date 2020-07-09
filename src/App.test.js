091f5e6bacbd443861c5d57ed7c89be2956faf2b
Abel Masila <abelmasila@gmail.com>
2020-07-09 21:57:05 +0300

    added tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,49 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import {
+  render,
+  screen,
+  findByAltText,
+  findAllByTitle,
+} from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import fetchMock from "jest-fetch-mock";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
+import data from "./__mocks__/vuesjs.json";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+fetchMock.enableMocks();
+function setup() {
+  return render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+}
+
+describe("Header", () => {
+  test("how it works link", () => {
+    setup();
+    //screen.debug();
+    const link = screen.getByRole("link", { name: /how it works/i });
+    //screen.debug(link);
+    userEvent.click(link);
+    expect(screen.findByRole("heading", { name: /how it works/i }));
+  });
+  test("test the about link", () => {
+    //write code
+    setup();
+  });
+});
+describe("Form", () => {
+  test("type in input, click button and see results", async () => {
+    fetch.once(JSON.stringify(data));
+    setup();
+    const textInput = screen.getByLabelText("r /");
+    userEvent.type(textInput, "vuejs");
+    //screen.debug();
+    const submitButton = screen.getByRole("button", { name: /search/i });
+    userEvent.click(submitButton);
+    expect(screen.getByText(/is loading/i)).toBeInTheDocument();
+    expect(await screen.findByText(/Number of top posts: 20/i));
+  });
 });
